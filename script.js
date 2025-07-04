4a8a242cb80938df03eb9f01d77fc5164b8a10ce
m-demarqui <maria.demarqui@escola.pr.gov.br>
2025-07-04 11:06:06 +0000

    aula 2

---
+++ script.js
@@ -0,0 +1,7 @@
+const botao MostraPalavras = document.querySelector('botao-palavrachave');
+
+botaoMostraPalavras.addEventListener('click,mostrapalavraschave');
+
+function mostrapalavraschave(){
+ alert('Deu boa Piazada');  
+}
